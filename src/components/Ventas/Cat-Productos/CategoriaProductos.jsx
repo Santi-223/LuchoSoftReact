35d198b1d283ba0930b71764c622e9f09f503e72
Juanda0125 <128322714+Juanda0125@users.noreply.github.com>
2024-06-16 23:08:33 -0500

    Update CategoriaProductos.jsx

--- src/components/Ventas/Cat-Productos/CategoriaProductos.jsx
+++ src/components/Ventas/Cat-Productos/CategoriaProductos.jsx
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router-dom"; // Agrega esta línea
 
 
 function CategoriasProductos() {
+    const token = localStorage.getItem("token");
     const navigate = useNavigate(); // Agrega esta línea
     const [categorias, setcategoria] = useState([]);
     const [estadoModaleditar, cambiarEstadoModalEditar] = useState(false);
@@ -77,6 +78,18 @@ function CategoriasProductos() {
                     }} className={estilos.boton} style={{ cursor: 'pointer', textAlign: 'center', fontSize: '20px' }}>
                         <i className={`fa-solid fa-pen-to-square ${row.estado_categoria_productos === 1 ? 'iconosVerdes' : 'iconosGris'}`}></i>
                     </button>
+                    <button
+                        onClick={() => handleEliminarCategoria(row.id_categoria_productos)}
+                        disabled={row.estado_categoria_productos === 0}
+                        className={estilos.boton}
+                        style={{ cursor: "pointer", textAlign: "center", fontSize: "25px" }}
+                    >
+                        <i
+                            className={`bi bi-trash ${row.estado_categoria_productos === 0 ? "basuraDesactivada" : ""
+                                }`}
+                            style={{ color: row.estado_categoria_productos === 0 ? "gray" : "red" }}
+                        ></i>
+                    </button>
 
                 </div>
             )
@@ -86,7 +99,30 @@ function CategoriasProductos() {
     const handleSubmitEditar = async (event) => {
         event.preventDefault();
     
-        Swal.fire({
+        // Validación del nombre de la categoría
+        if (!categoriasEditar.nombre_categoria_productos) {
+            await Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Por favor completa el nombre de la categoría.',
+                confirmButtonText: 'Aceptar'
+            });
+            return;
+        }
+    
+        const regex = /^[a-zA-Z0-9.,?!¡¿\s]+$/;
+        if (!regex.test(categoriasEditar.nombre_categoria_productos)) {
+            await Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Evita caracteres especiales en el nombre de la categoría.',
+                confirmButtonText: 'Aceptar'
+            });
+            return;
+        }
+    
+        // Confirmación de actualización
+        const { isConfirmed } = await Swal.fire({
             title: '¿Estás seguro?',
             text: '¿Deseas actualizar la información de la categoría de producto?',
             icon: 'warning',
@@ -95,75 +131,54 @@ function CategoriasProductos() {
             cancelButtonColor: '#d33',
             confirmButtonText: 'Sí',
             cancelButtonText: 'Cancelar'
-        }).then(async (result) => {
-
-            if (!categorias1.nombre_categoria_productos) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Por favor completa el nombre de la categoría.',
-                    confirmButtonText: 'Aceptar'
-                });
-                return;
-            }
-        
-            const regex = /^[a-zA-Z0-9.,?!¡¿\s]+$/;
-        
-            if (!regex.test(categorias1.nombre_categoria_productos)) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Evita caracteres especiales en el nombre de la categoría.',
-                    confirmButtonText: 'Aceptar'
-                });
-                return;
-            }
-
-            if (result.isConfirmed) {
-                try {
-                    const response = await fetch(`https://api-luchosoft-mysql.onrender.com/ventas2/categoria_productos/${categoriasEditar.id_categoria_productos}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(categoriasEditar)
-                    });
-    
-                    if (response.ok) {
-                        console.log('Categoría de producto actualizada exitosamente.');
-    
-                        Swal.fire({
-                            toast: true,
-                            position: "top-end",
-                            showConfirmButton: false,
-                            timer: 1500,
-                            timerProgressBar: true,
-                            icon: "success",
-                            title: "Actualización exitosa"
-                        }).then(() => {
-                            cambiarEstadoModalEditar(false); // Cierra el modal de edición
-                            navigate('/categoria_productos');
-                            window.location.reload(); // Recarga la página
-                        });
-                    } else {
-                        console.error('Error al actualizar la categoría de producto:', response.statusText);
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Error',
-                            text: 'Error al actualizar la categoría de producto',
-                        });
-                    }
-                } catch (error) {
-                    console.error('Error al actualizar la categoría de producto:', error);
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Error al actualizar la categoría de producto',
-                    });
-                }
-            }
         });
+    
+        if (!isConfirmed) return;
+    
+        // Intentar actualizar la categoría
+        try {
+            const response = await fetch(`https://api-luchosoft-mysql.onrender.com/ventas2/categoria_productos/${categoriasEditar.id_categoria_productos}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(categoriasEditar),
+            });
+    
+            if (response.ok) {
+                console.log('Categoría de producto actualizada exitosamente.');
+                await Swal.fire({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 1500,
+                    timerProgressBar: true,
+                    icon: 'success',
+                    title: 'Actualización exitosa'
+                });
+    
+                cambiarEstadoModalEditar(false); // Cierra el modal de edición
+                navigate('/categoria_productos');
+                window.location.reload(); // Recarga la página
+            } else {
+                const errorText = await response.text();
+                console.error('Error al actualizar la categoría de producto:', errorText);
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: `Error al actualizar la categoría de producto: ${response.statusText}`,
+                });
+            }
+        } catch (error) {
+            console.error('Error al actualizar la categoría de producto:', error);
+            await Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: `Error al actualizar la categoría de producto: ${error.message}`,
+            });
+        }
     };
+    
     
 
     useEffect(() => {
@@ -194,6 +209,93 @@ function CategoriasProductos() {
             console.error('Error al obtener las categorías de producto:', error);
         }
     };
+
+    const handleEliminarCategoria = async (idCategoria) => {
+        console.log("Intentando eliminar la categoría con ID:", idCategoria);
+    
+        try {
+            // Obtener todos los productos y filtrar por la categoría
+            const productosResponse = await fetch('https://api-luchosoft-mysql.onrender.com/ventas2/productos/', {
+                headers: {
+                    "Content-Type": "application/json",
+                    token: token,
+                },
+            });
+    
+            if (!productosResponse.ok) {
+                throw new Error('Error al verificar los productos de la categoría.');
+            }
+    
+            const productos = await productosResponse.json();
+    
+            // Filtrar productos que pertenecen a la categoría
+            const productosEnCategoria = productos.filter(producto => producto.id_categoria_producto === idCategoria);
+    
+            if (productosEnCategoria.length > 0) {
+                await Swal.fire({
+                    icon: "error",
+                    title: "No se puede eliminar",
+                    text: "La categoría tiene productos asociados. Elimínelos primero.",
+                });
+                return;
+            }
+    
+            // Mostrar mensaje de confirmación
+            const { isConfirmed } = await Swal.fire({
+                text: "¿Deseas eliminar esta categoría?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                confirmButtonText: "Sí, eliminar",
+                cancelButtonText: "Cancelar",
+            });
+    
+            if (!isConfirmed) return;
+    
+            console.log("Confirmación recibida para eliminar la categoría.");
+            console.log("Token utilizado:", token);
+    
+            // Solicitud DELETE
+            const response = await fetch(`https://api-luchosoft-mysql.onrender.com/ventas2/categoria_productos/${idCategoria}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    token: token,
+                },
+            });
+    
+            // Manejo de la respuesta
+            if (response.ok) {
+                console.log("Categoría eliminada exitosamente.");
+                await Swal.fire({
+                    icon: "success",
+                    title: "Categoría eliminada",
+                    text: "La categoría ha sido eliminada correctamente",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                fetchcategorias();
+            } else {
+                const errorText = await response.text();
+                console.error("Error al eliminar la categoría:", response.status, errorText);
+                await Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: `Error al eliminar la categoría: ${response.statusText}`,
+                });
+            }
+        } catch (error) {
+            console.error("Error al eliminar la categoría:", error);
+            await Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: `Error al eliminar la categoría: ${error.message}`,
+            });
+        }
+    };
+    
+    
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -355,6 +457,7 @@ function CategoriasProductos() {
             <div className={estilos['divFiltro']}>
                 <input type="text" placeholder=" Buscar..." value={filtro} onChange={handleFiltroChange} className={estilos["busqueda"]} />
                 <div>                
+
                     <button onClick={() => cambiarEstadoModalAgregar(!estadoModalAgregar)} className={` ${estilos.botonAgregar} ${estilos.rojo} bebas-neue-regular`}><i className="fa-solid fa-plus"></i> Agregar</button>
 
                 </div>
