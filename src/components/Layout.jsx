c30dc84d21c02af92073638bdd8bc8d789c44dd0
Santi-223 <santitsj223munera@gmail.com>
2024-06-17 21:58:51 -0500

    agreagar de nuevo la importacion globalmente

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -177,7 +177,7 @@ const Layout = () => {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
             <link href="https://cdn.datatables.net/2.0.2/css/dataTables.semanticui.css" rel="stylesheet" />
-
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.9.2/semantic.min.css" rel="stylesheet" />
 
             <div className="barraPrincipal">
                 <div className="contenedor_1">
